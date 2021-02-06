c3a6f081d280279cf2c8c1f57e9a2d15cb570150
Charles <charlesboy49@gmail.com>
2021-02-06 19:46:52 +0100

    Update base.py

--- glc_project/settings/base.py
+++ glc_project/settings/base.py
@@ -79,7 +79,7 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'AUTH_TOKEN_CLASSES': (
             'rest_framework_simplejwt.tokens.AccessToken',
-            rest_framework_simplejwt.tokens.SlidingToken',
+            'rest_framework_simplejwt.tokens.SlidingToken',
         ),
 }
 
