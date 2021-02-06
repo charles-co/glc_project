24fac090e2b6c4a89e6ad067bba63a5aeefcc846
Charles <charlesboy49@gmail.com>
2021-02-06 20:42:50 +0100

    Update production.py

--- glc_project/settings/production.py
+++ glc_project/settings/production.py
@@ -11,15 +11,15 @@ EMAIL_PORT = os.environ.get("MAILGUN_SMTP_PORT", "")
 # EMAIL_USE_SSL = True # Yes for Gmail
 DEFAULT_FROM_EMAIL = "GLC <webmaster@localhost>"
 
-#CORS_REPLACE_HTTPS_REFERER      = True
-#HOST_SCHEME                     = "https://"
-#SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
-#SECURE_SSL_REDIRECT             = True
-#SESSION_COOKIE_SECURE           = True
-#CSRF_COOKIE_SECURE              = True
-#SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
-#SECURE_HSTS_SECONDS             = 1000000
-# SECURE_FRAME_DENY               = True
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
 
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
