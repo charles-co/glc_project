8b4d41b3a56a2eb3093adb21198bb539972a4972
charles-co <charlesboy49@gmail.com>
2021-02-11 11:06:54 +0100

    edits

--- authentication/apps.py
+++ authentication/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class AuthenticationConfig(AppConfig):
     name = 'authentication'
-    icon_name = 'manage_accounts'
+    icon_name = 'person_add'
 
     def ready(self):
         import authentication.signals
--- glc_project/settings/base.py
+++ glc_project/settings/base.py
@@ -65,23 +65,24 @@ INSTALLED_APPS = [
 ]
 
 # X_FRAME_OPTIONS ='SAMEORIGIN'
+# from django.templatetags.static import static
 
 MATERIAL_ADMIN_SITE = {
     'HEADER':  _('GLC'),  # Admin site header
     'TITLE':  _('GLC'),  # Admin site title
-    'FAVICON':  '/image/church.png',  # Admin site favicon (path to static should be specified)
+    'FAVICON':  '/image/church.png/',  # Admin site favicon (path to static should be specified)
     'MAIN_BG_COLOR':  '#F15922',  # Admin site main color, css color should be specified
     'MAIN_HOVER_COLOR':  '#FFC50C',  # Admin site main hover color, css color should be specified
-    'PROFILE_PICTURE':  '/image/logo.jpg',  # Admin site profile picture (path to static should be specified)
-    'PROFILE_BG':  '/image/background3.jpg',  # Admin site profile background (path to static should be specified)
-    'LOGIN_LOGO':  '/image/logo.jpg',  # Admin site logo on login page (path to static should be specified)
-    'LOGOUT_BG':  '/image/background.jpeg',  # Admin site background on login/logout pages (path to static should be specified)
+    'PROFILE_PICTURE':  '/image/logo.jpg/',  # Admin site profile picture (path to static should be specified)
+    'PROFILE_BG':  '/image/background3.jpg/',  # Admin site profile background (path to static should be specified)
+    'LOGIN_LOGO':  '/image/logo.jpg/',  # Admin site logo on login page (path to static should be specified)
+    'LOGOUT_BG':  '/image/background.jpeg/',  # Admin site background on login/logout pages (path to static should be specified)
     'SHOW_THEMES':  True,  #  Show default admin themes button
     'TRAY_REVERSE': True,  # Hide object-tools and additional-submit-line by default
     'NAVBAR_REVERSE': True,  # Hide side navbar by default
     'SHOW_COUNTS': True, # Show instances counts for each model
     'APP_ICONS': {  # Set icons for applications(lowercase), including 3rd party apps, {'application_name': 'material_icon_name', ...}
-        'authentication': 'manage_accounts',
+        'authentication_and_authorization': 'verified',
     },
     'MODEL_ICONS': {  # Set icons for models(lowercase), including 3rd party models, {'model_name': 'material_icon_name', ...}
         'user': 'account_circle',
@@ -128,13 +129,13 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [TEMPLATE_DIR],
-        # 'APP_DIRS': True,
+        'APP_DIRS': True,
         'OPTIONS': {
-            'loaders': [
-                'apptemplates.Loader',
-                'django.template.loaders.filesystem.Loader',
-                'django.template.loaders.app_directories.Loader',
-            ],
+            # 'loaders': [
+            #     'apptemplates.Loader',
+            #     'django.template.loaders.filesystem.Loader',
+            #     'django.template.loaders.app_directories.Loader',
+            # ],
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
@@ -269,7 +270,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+   os.path.join(BASE_DIR, 'static'),
+)
