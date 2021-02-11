9fd6740b68acb72c279938c7505581aedfe594d3
Charles <charlesboy49@gmail.com>
2021-02-11 13:28:59 +0100

    Update production.py

--- glc_project/settings/production.py
+++ glc_project/settings/production.py
@@ -29,4 +29,4 @@ STATICFILES_DIRS = (
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-django_heroku.settings(config=locals(), staticfiles=False, logging=False)
+django_heroku.settings(locals())
