3d281215fcfe0f07d911f01d70e37bdca5c483c4
Charles <charlesboy49@gmail.com>
2021-02-11 13:01:28 +0100

    Update base.py

--- glc_project/settings/base.py
+++ glc_project/settings/base.py
@@ -70,13 +70,13 @@ INSTALLED_APPS = [
 MATERIAL_ADMIN_SITE = {
     'HEADER':  _('GLC'),  # Admin site header
     'TITLE':  _('GLC'),  # Admin site title
-#    'FAVICON':  'image/church.png',  # Admin site favicon (path to static should be specified)
+    'FAVICON':  'image/church.png',  # Admin site favicon (path to static should be specified)
     'MAIN_BG_COLOR':  '#F15922',  # Admin site main color, css color should be specified
     'MAIN_HOVER_COLOR':  '#FFC50C',  # Admin site main hover color, css color should be specified
-#    'PROFILE_PICTURE':  'image/logo.jpg',  # Admin site profile picture (path to static should be specified)
-#   'PROFILE_BG':  'image/background3.jpg',  # Admin site profile background (path to static should be specified)
-#    'LOGIN_LOGO':  'image/logo.jpg',  # Admin site logo on login page (path to static should be specified)
-#    'LOGOUT_BG':  'image/background.jpeg',  # Admin site background on login/logout pages (path to static should be specified)
+    'PROFILE_PICTURE':  'image/logo.jpg',  # Admin site profile picture (path to static should be specified)
+    'PROFILE_BG':  'image/background3.jpg',  # Admin site profile background (path to static should be specified)
+    'LOGIN_LOGO':  'image/logo.jpg',  # Admin site logo on login page (path to static should be specified)
+    'LOGOUT_BG':  'image/background.jpeg',  # Admin site background on login/logout pages (path to static should be specified)
     'SHOW_THEMES':  True,  #  Show default admin themes button
     'TRAY_REVERSE': False,  # Hide object-tools and additional-submit-line by default
     'NAVBAR_REVERSE': False,  # Hide side navbar by default
