50a203045d52d6c108e0fdf3a362b9228e87d668
Charles <charlesboy49@gmail.com>
2021-02-11 14:09:53 +0100

    Update base.py

--- glc_project/settings/base.py
+++ glc_project/settings/base.py
@@ -75,7 +75,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
