0c98f45d9cdd4b79b44cc468975156facf0c4213
charles-co <charlesboy49@gmail.com>
2021-02-11 09:37:53 +0100

    edits

--- authentication/apps.py
+++ authentication/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class AuthenticationConfig(AppConfig):
     name = 'authentication'
-    icon_name = 'person'
+    icon_name = 'manage_accounts'
 
     def ready(self):
         import authentication.signals
