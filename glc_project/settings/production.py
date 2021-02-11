ae8b5d592209f5b411026fc2c8261f0a1df2d6a5
Charles <charlesboy49@gmail.com>
2021-02-11 12:59:28 +0100

    Update production.py

--- glc_project/settings/production.py
+++ glc_project/settings/production.py
@@ -23,9 +23,9 @@ SECURE_FRAME_DENY               = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-# )
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
